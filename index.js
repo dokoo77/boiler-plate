45b504feb7620dbb5a09d63109d7e09a1160ce56
jae-seok-lee <jae-seok.lee@kr.ey.com>
2021-03-29 20:43:29 +0900

    Add key.js

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const {User} = require("./models/User")
+const config = require('./config/key');
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://dokoo77:qwerasdf@cluster0.xnofd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, userUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!!!')
+})
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err })
+        console.log(userInfo);
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
