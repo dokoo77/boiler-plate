521602d682a2dd52e7f38713b8df900d03e63874
jae-seok-lee <jae-seok.lee@kr.ey.com>
2021-03-30 16:29:38 +0900

    Add register, login

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
-const {User} = require("./models/User")
+const cookieParser = require('cookie-parser');
+const { User } = require("./models/User")
 const config = require('./config/key');
 
 // application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
@@ -29,6 +31,38 @@ app.post('/register', (req, res) => {
             success: true
         })
     })
+})
+
+app.post('/login', (req, res) => {
+
+    // Find the email address in my DB
+    User.findOne({ email: req.body.email}, (err, user) => {
+        if(!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "The email address is not exist"
+            })
+        }
+
+        console.log("comparePassword");
+        // Find the password in my DB
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch) return res.json({loginSuccess: false, message: 'The password is not exist'});
+
+            console.log('Password is exist');
+
+            // Generate token
+            user.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);
+
+                // Save the token to cookie.
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({loginSuccess: true, userId: user._id});
+            })
+        })
+    })
+
 })
 
 app.listen(port, () => {
